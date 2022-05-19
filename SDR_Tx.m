cec7c34d7925ec645ffa99f38da22963ab470512
Benjamin Havens <benjaminhavens@byu.net>
2022-05-18 18:27:40 -0600

    Update tx to while loop and correct sampling rate. Deleted screenshots folder

--- SDR_Tx.m
+++ SDR_Tx.m
@@ -1,19 +1,27 @@
+%%
 clc;
 
 fc = 1485e6;
 f1 = 1486e6;
 ts = 1/(20e6);
 t = 0 : ts : 1/(f1 - fc) - ts;
-x_tilde = exp(1j*2*pi*(f1 - fc)*t);
+t = t';
+x_tilde_once = exp(1j*2*pi*(f1 - fc)*t);
+x_tilde = repmat(x_tilde_once, [1e5 1]);
 
 if ~(exist('tx', 'var'))
     tx = comm.SDRuTransmitter(              ...
                   'Platform','B210',        ...
                   'SerialNum','3218C97',    ...
                   'CenterFrequency',fc,     ...
-                  'MasterClockRate',1/ts);
+                  'MasterClockRate',1/ts,   ...
+                  'InterpolationFactor',1);
 end
 
-for i = 1:10e3
-    tx(x_tilde');
+%%
+while 1
+    tx(x_tilde);
 end
+
+%%
+release(tx);
