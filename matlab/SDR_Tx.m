a161ef556026fa53a85fd6e385319fcee89301aa
Preston-Walker <pw.prestonwalker@gmail.com>
2022-06-14 22:23:58 -0600

    changed tx code

--- matlab/SDR_Tx.m
+++ matlab/SDR_Tx.m
@@ -1,12 +1,12 @@
 %% Initialize
 fc = 1485e6;
 f1 = 1486e6;
-ts = 1/(10e6);
+ts = 1/(20e6);
 %t = 0 : ts : 1/(f1 - fc) - ts;
 %t = t';
 %x_tilde_once = exp(1j*2*pi*(f1 - fc)*t);
 load soundingSignal-BW10-Fs20.mat
-x_tilde_once = xl;
+x_tilde_once = xl/10;
 x_tilde = repmat(x_tilde_once, [1e3 1]);
 
 if ~(exist('tx', 'var'))
@@ -17,9 +17,10 @@ if ~(exist('tx', 'var'))
                   'MasterClockRate',1/ts,   ...
                   'InterpolationFactor',1);
 end
-tx.Gain = 40;
+tx.Gain = 60;
 
 %% Transmit until stopped
+info(tx)
 disp('Transmitting')
 while 1
     tx(x_tilde);
