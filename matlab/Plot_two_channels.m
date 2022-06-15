28264710386c0d5ee4dd7b3292c23fc4a9bd52b4
rskirkwood <rskirkwood6@gmail.com>
2022-06-15 16:08:40 -0600

    updated matlab plot code

--- matlab/Plot_two_channels.m
+++ matlab/Plot_two_channels.m
@@ -11,9 +11,9 @@
 % data_b = M(:, 2);
 
 
-num_samples = 1000;
+num_samples = 128;
 
-mdrIdx = 50001;
+mdrIdx = 5000;
 
 
 % CHANNEL A PLOTS
@@ -24,10 +24,6 @@ title("Channel A"); xlabel("Sample number"); ylabel("Amplitude");
 legend("Real", "Imaginary");
 grid on;
 
-% figure(2);  % Plots real vs imaginary to see periodicity
-% plot(data_a(1:100), '.-'); 
-% grid on;
-
 % Prepare to plot fft
 Nfft = 128;
 FF = -0.5 : 1/Nfft : 0.5-1/Nfft;
@@ -35,7 +31,7 @@ X1 = fft(data_a(mdrIdx:mdrIdx+Nfft-1), Nfft);
 X2 = fftshift(X1);
 [max_val, max_idx] = max(abs(X2));
 
-figure(3);  % Has FFT plot
+figure(2);  % Has FFT plot
 stem(FF, abs(X2));
 title("Channel A FFT");
 xlabel('F normalized frequency (cycles/sample)');
@@ -44,27 +40,22 @@ mystring = sprintf('   max occurs at F = %4.3f cycles/sample', FF(max_idx));
 text(FF(max_idx),max_val,mystring);
 grid on;
 
-return;
-
 % CHANNEL B PLOTS
-figure(4);  % This has the pure sines and cosines
-plot(0 : num_samples - 1, real(data_b(1:num_samples)), '.-',        ...
-     0 : num_samples - 1, imag(data_b(1:num_samples)), '.-'); 
+figure(3);  % This has the pure sines and cosines
+plot(mdrIdx : mdrIdx + num_samples - 1, real(data_b(mdrIdx:mdrIdx + num_samples -1)), '.-',        ...
+     mdrIdx : mdrIdx + num_samples - 1, imag(data_b(mdrIdx:mdrIdx + num_samples -1)), '.-'); 
 title("Channel B"); xlabel("Sample number"); ylabel("Amplitude");
 legend("Real", "Imaginary");
 grid on;
 
-figure(5);  % Plots real vs imaginary to see periodicity
-plot(data_b(1:num_samples) ,'.-'); 
-grid on;
 
 % Prepare to plot fft
 X1 = fft(data_b, Nfft);
 X2 = fftshift(X1);
 [max_val, max_idx] = max(abs(X2));
 
-figure(6);  % Has FFT plot
-plot(FF, abs(X2));
+figure(4);  % Has FFT plot
+stem(FF, abs(X2));
 title("Channel B FFT");
 xlabel('F normalized frequency (cycles/sample)');
 ylabel('magnitude');
