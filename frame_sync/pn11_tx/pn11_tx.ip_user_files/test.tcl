f680ec89702db4281d1b31924a19d91581748820
Preston Walker <pw.prestonwalker@gmail.com>
2022-07-14 16:03:45 -0600

    added sv pn11 file

---
+++ frame_sync/pn11_tx/pn11_tx.ip_user_files/test.tcl
@@ -0,0 +1,10 @@
+add_force clk {0 0} {1 5ns} -repeat_every 10ns
+
+add_force clr 1
+
+run 100 ns
+
+add_force clr 0
+add_force start 1
+
+run 50000ns
---
+++ frame_sync/pn11_tx/pn11_tx.srcs/sources_1/new/generate_PN11.sv
@@ -0,0 +1,142 @@
+`timescale 1ns / 1ps
+`default_nettype none
+//////////////////////////////////////////////////////////////////////////////////
+// Company: ICE lab
+// Engineer: Preston Walker
+// 
+// Create Date: 07/14/2022 12:43:57 PM
+// Design Name: 
+// Module Name: generate_PN11
+// Project Name: PN11 tx
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module generate_PN11(
+    input wire logic clk,
+    input wire logic start,
+    input wire logic clr,
+    output logic pn11_bit
+    );
+    logic tmr_done, pilot_reset, pilot_done, counter_reset;
+    logic[10:0] register = 11'b00000000001;
+    logic[11:0] counter3200 = 0; //4096 bits
+    logic[10:0] counter2048 = 0; //2048 bits
+    logic[6:0] counter128 = 0; //128 bits
+    typedef enum logic[1:0] {idle, pilot, PN11, ERR='X}StateType;
+    StateType ns,cs;
+    
+    
+    always_comb
+        begin
+        ns = ERR;
+        pilot_reset = 0;
+        counter_reset = 0;
+        
+        if(clr)
+            ns = idle;
+        else
+            case(cs)
+                idle: 
+                    begin
+                        if(start)
+                            begin 
+                            ns = pilot;
+                            counter128 = 0;
+                            end
+                        else
+                            ns = idle;
+                    end
+                PN11: if(tmr_done)
+                        ns = pilot;
+                    else
+                        begin
+                        pn11_bit = register[0];
+                        
+                        pilot_reset = 1;
+                        ns = PN11;
+                        if(counter2048 != 0)
+                            begin
+                        
+                            register[0] = register[1];  
+                            register[1] = register[2];
+                            register[2] = register[3];
+                            register[3] = register[4];
+                            register[4] = register[5];
+                            register[5] = register[6];
+                            register[6] = register[7];
+                            register[7] = register[8];
+                            register[8] = pn11_bit^register[9];
+                            register[9] = register[10];
+                            register[10] = pn11_bit;
+                            end
+
+
+                                               
+                        end
+                        
+                pilot:
+                    if (pilot_done)
+                        begin
+                        ns = PN11;
+                        pilot_reset = 1;
+                        end
+                    else 
+                        ns = pilot;
+                        
+            endcase
+        end
+    
+    //state switch
+    always_ff@(posedge clk)
+        cs <= ns;
+    
+    //counter
+    always_ff@(posedge clk)
+    begin
+        if(pilot_reset)
+            begin
+            counter128 <= 0;
+            if (counter2048 <= 2046)
+                begin
+                counter2048 = counter2048 + 1;
+                end
+            else
+                begin
+                counter2048 <= 0;
+                register <= 11'b00000000001;
+                end 
+            if (counter3200 <= 3199)
+                begin
+                counter3200 = counter3200 + 1;
+                tmr_done <= 0;
+                end
+            else
+                begin
+                counter3200 <= 0;
+                tmr_done <= 1;
+                end
+            end
+        else
+            begin
+            if (counter128 <= 126)
+                begin
+                counter128 <= counter128 + 1;
+                pilot_done <= 0;
+                end
+            else
+                begin
+                pilot_done <= 1;
+                end
+            end
+        end
+endmodule
