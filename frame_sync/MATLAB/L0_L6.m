428c6722f7c61f2eac8054afd9db9a8ecfb5383b
Benjamin Havens <benjaminhavens@byu.net>
2022-08-05 15:16:39 -0600

    Correlation function L6q first draft

--- frame_sync/MATLAB/L0_L6.m
+++ frame_sync/MATLAB/L0_L6.m
@@ -1,7 +1,8 @@
 %%%%%%%%%%%
 % L0_L6.m %
 %%%%%%%%%%%
-% Uses the frame synchronizer functions from 
+% Uses the frame synchronizer functions from Rice-McMurdie 2016
+% Needs a variable data_a
 
 load preambleTemplate.mat
 rev_preamble = flip(preamble_template);
@@ -9,18 +10,71 @@ rev_preamble = flip(preamble_template);
 %%%%%%
 % L0 %
 %%%%%%
-L0_corr = conv(data_a(50e3:end), conj(rev_preamble));
-L0_corr = abs(L0_corr);
-L0_corr = L0_corr(1:10000);
+L0_res = conv(data_a(50e3:end), conj(rev_preamble));
+L0_res = abs(L0_res);
+L0_res = L0_res(1:10000);
 % L0_corr = 20*log10(L0_corr);
 
 figure(1); 
-subplot(211); plot(L0_corr); grid on;
-subplot(212); plot(movmean(L0_corr, 80)); grid on;
+subplot(211); plot(L0_res); grid on;
+subplot(212); plot(movmean(L0_res, 80)); grid on;
 
 %%%%%%%
 % L6q %
 %%%%%%%
-figure(1); 
-subplot(211); plot(L0_corr); grid on;
-subplot(212); plot(movmean(L0_corr, 80)); grid on;
+L6q_res = zeros(1e6, 1);
+for m = 1:1e6
+    L6q_res(m) = L6q_corr(m, data_a);
+end
+L6q_res = L6q_res(1e5:1.1e5);
+
+figure(6);
+subplot(211); plot(L6q_res); grid on;
+subplot(212); plot(movmean(L6q_res, 100)); grid on;
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%
+% FUNCTION DECLARATIONS %
+%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function L6q = L6q_corr(m, data_in)
+    L1 = [0,8,16,24] + 1;
+    L2 = [4,20] + 1;
+    L3 = [2,10,14,22] + 1;
+    L4 = [6,18,26,30] + 1;
+    L5 = [1,7,9,15,17,23,25,31] + 1;
+    L6 = [3,5,11,12,13,19,21,27,28,29] + 1;
+    L7 = [1,3,9,11,12,13,15,21,23] + 1;
+    L8 = [5,7,17,19,25,27,28,29,31] + 1;
+    data_r = real(data_in);
+    data_i = imag(data_in);
+    sum = 0;
+    for i = 0:7
+        I = sum(data_r(L1 + 32*i + m)) + ...
+            -sum(data_r(L2 + 32*i + m)) + ...
+            sum(data_i(L3 + 32*i + m)) + ...
+            -sum(data_i(L4 + 32*i + m)) + ...
+            1/sqrt(2)*( ...
+            sum(data_r(L5 + 32*i + m)) + ...
+            -sum(data_r(L6 + 32*i + m)) + ...
+            sum(data_i(L7 + 32*i + m)) + ...
+            -sum(data_i(L8 + 32*i + m)) ...
+            );
+        Q = sum(data_i(L1 + 32*i + m)) + ...
+            -sum(data_i(L2 + 32*i + m)) + ...
+            sum(data_r(L3 + 32*i + m)) + ...
+            -sum(data_r(L4 + 32*i + m)) + ...
+            1/sqrt(2)*( ...
+            sum(data_i(L5 + 32*i + m)) + ...
+            -sum(data_i(L6 + 32*i + m)) + ...
+            sum(data_r(L7 + 32*i + m)) + ...
+            -sum(data_r(L8 + 32*i + m)) ...
+            );
+        sum = sum + I^2 + Q^2;
+    end
+    L6q = sum;
+end
+
+
+
+
