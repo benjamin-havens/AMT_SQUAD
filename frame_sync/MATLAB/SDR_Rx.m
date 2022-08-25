8da961559640934d56c84ce8667ec4b89124c0f8
rskirkwood <101432150+rskirkwood@users.noreply.github.com>
2022-08-25 11:55:23 -0600

    copied receive file to fram_sync folder

---
+++ frame_sync/MATLAB/SDR_Rx.m
@@ -0,0 +1,30 @@
+%% Initialize
+ts = 1/(20.8e6);
+if(~(exist('rx', 'var')))
+    rx = comm.SDRuReceiver();
+    rx.Platform = "B210";
+    rx.SerialNum = '3218C97';
+    rx.ChannelMapping = [1 2];
+    rx.CenterFrequency = 1485e6;
+    rx.MasterClockRate = (1/ts);
+    rx.Gain = 20;
+    rx.DecimationFactor = 1;
+end
+rx.SamplesPerFrame = 1e6;
+
+%% Receive
+rxLog = dsp.SignalSink;
+disp('receiving')
+data = rx();
+info(rx)
+rxLog(data);
+data = rxLog.Buffer;
+data_a = data(:, 1);
+data_b = data(:, 2);
+% writematrix(data, '');
+
+%% Clean up
+release(rx);
+release(rxLog);
+
+
